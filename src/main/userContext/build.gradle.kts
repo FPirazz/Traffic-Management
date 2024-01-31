fa34ff2dc3b0cda0d1bbaf6a00434772fca4b912
FPirazz <federico.pirazzoli@studio.unibo.it>
2024-01-31 21:48:18 +0100

    build: Configured build.gradle inside userContext module to contain the right dependencies used to implement a microservice using Spring

--- src/main/userContext/build.gradle.kts
+++ src/main/userContext/build.gradle.kts
@@ -1,19 +1,27 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
