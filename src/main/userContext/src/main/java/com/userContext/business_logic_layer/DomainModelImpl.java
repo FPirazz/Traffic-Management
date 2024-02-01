590b1538d2a1f0f8009f804ffcf48389c599b30d
FPirazz <federicopirazzoli99@gmail.com>
2024-02-01 15:02:39 +0100

    Implemented DataSourcePort and its exception inside the business logic, amongst with appropriate changes in the User and DomainModelImpl

--- src/main/userContext/src/main/java/com/userContext/business_logic_layer/DomainModelImpl.java
+++ src/main/userContext/src/main/java/com/userContext/business_logic_layer/DomainModelImpl.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 
 public class DomainModelImpl implements DomainModel{
 
+    private static DataSourcePort dataSourcePort;
     private Map<String, User> users;
     static Logger logger = Logger.getLogger("[DomainModel]");
     @Override
@@ -13,18 +14,28 @@ public class DomainModelImpl implements DomainModel{
 
     }
 
+    public void init(DataSourcePort port) {
+        this.dataSourcePort = port;
+    }
+
     @Override
     public Optional<User> getUserById(String userId) {
         return Optional.empty();
     }
-
     @Override
     public Optional<User> getUserByName(String userName, String userSurname) {
         return Optional.empty();
     }
-
     @Override
     public Optional<User> getUserByRole(String userRole) {
         return Optional.empty();
+    }
+
+    public static DataSourcePort getDataSourcePort() {
+        return dataSourcePort;
+    }
+
+    public Map<String, User> getUsers() {
+        return users;
     }
 }
