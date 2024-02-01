a3df2b4d8bce0faa128ea398defdeba7d9b82d18
FPirazz <federicopirazzoli99@gmail.com>
2024-02-01 15:54:14 +0100

    Added ApplicationAPI and its implementation on the application layer

--- src/main/userContext/src/main/java/com/userContext/business_logic_layer/DataSourcePort.java
+++ src/main/userContext/src/main/java/com/userContext/business_logic_layer/DataSourcePort.java
@@ -1,5 +1,7 @@
 package com.userContext.business_logic_layer;
 
+import com.userContext.business_logic_layer.exceptions.DataSourceException;
+
 public interface DataSourcePort {
 
     void saveUser() throws DataSourceException;
--- src/main/userContext/src/main/java/com/userContext/business_logic_layer/exceptions/DataSourceException.java
+++ src/main/userContext/src/main/java/com/userContext/business_logic_layer/exceptions/DataSourceException.java
@@ -1,4 +1,4 @@
-package com.userContext.business_logic_layer;
+package com.userContext.business_logic_layer.exceptions;
 
 public class DataSourceException extends Exception {
     public DataSourceException(final String msg) {super(msg);}
