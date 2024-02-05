4cdf2831cc7d218e0d62c2b008e8c2ab455c265a
FPirazz <federicopirazzoli99@gmail.com>
2024-02-05 18:49:12 +0100

    feat: Now traffic lights work in pairs (1 and 3, 2 and 4), simulating a real life intersection, every n amount of seconds

--- intersectionAggregate/src/env/acme/Intersection.java
+++ intersectionAggregate/src/env/acme/Intersection.java
@@ -11,10 +11,12 @@ import java.util.List;
 public class Intersection extends Artifact {
 
     private List<String> ids;
+    private List<String> tempIds;
 
 
     void init() {
         this.ids = new ArrayList<>();
+        this.tempIds = new ArrayList<>();
         log("Intersection is ready");
     }
 
@@ -24,6 +26,33 @@ public class Intersection extends Artifact {
         log("Added TR Id: " + id);
         if(ids.size() == 4) {
             Collections.sort(ids);
+            this.startTrafficLight(ids);
+        }
+    }
+
+    @OPERATION
+    public void trIsDone(final String id) {
+        this.tempIds.add(id);
+        this.ids.remove(id);
+
+//        log("ID " + id + " arrived");
+//        log(String.valueOf(tempIds.size()));
+//        log(String.valueOf(ids.size()));
+
+        if(tempIds.size() == 2 && ids.size() == 2) {
+            Collections.sort(tempIds);
+            try {
+                ArtifactId trafficLight = this.lookupArtifact("traffic_light_" + ids.get(0));
+                execLinkedOp(trafficLight, "preGreenState");
+
+                trafficLight = this.lookupArtifact("traffic_light_" + ids.get(1));
+                execLinkedOp(trafficLight, "preGreenState");
+            } catch (OperationException e) {
+                throw new RuntimeException(e);
+            }
+            ids.addAll(tempIds);
+            Collections.sort(ids);
+            tempIds.clear();
         }
     }
 
@@ -31,7 +60,11 @@ public class Intersection extends Artifact {
     private void startTrafficLight(final List<String> ids) {
         try {
             ArtifactId trafficLight = this.lookupArtifact("traffic_light_" + ids.get(0));
-            execLinkedOp(trafficLight, "greenState");
+            execLinkedOp(trafficLight, "preGreenState");
+
+            trafficLight = this.lookupArtifact("traffic_light_" + ids.get(2));
+            execLinkedOp(trafficLight, "preGreenState");
+
         } catch (OperationException e) {
             throw new RuntimeException(e);
         }
