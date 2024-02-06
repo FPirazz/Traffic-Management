d49a8760f34a1866de4751637f06eda15ec78bca
FPirazz <federicopirazzoli99@gmail.com>
2024-02-06 13:44:17 +0100

    feat: Decided to move the lanes to each TrafficLight class, and also implemented a function to spawn vehicles randomly, and count them up

--- intersectionAggregate/src/env/acme/TrafficLight.java
+++ intersectionAggregate/src/env/acme/TrafficLight.java
@@ -2,17 +2,32 @@ package acme;
 
 import cartago.*;
 
+import java.util.Random;
+
 public class TrafficLight extends Artifact {
 
     private String id;
     private TrafficLightState state;
+    private Lane lane;
+    private long redWaitTime;
+    private long yellowWaitTime;
+    private long greenWaitTime;
+
 
     void init(final String id) {
-        defineObsProperty("trState" + id, "red");
-        defineObsProperty("target_state" + id, "red");
         this.state = TrafficLightState.RED;
         this.id = id;
-//        log(id + " - Traffic Light RED");
+        this.lane = new Lane();
+        redWaitTime = 5_000;
+        yellowWaitTime = 5_000;
+        greenWaitTime = 5_000;
+
+        defineObsProperty("trState" + id, "red");
+        defineObsProperty("normalVehicles", 0);
+        defineObsProperty("emergencyVehicles", 0);
+        defineObsProperty("totalVehicles", 0);
+        defineObsProperty("target_state" + id, "red");
+
         log("Traffic Light is ready");
     }
 
@@ -21,7 +36,7 @@ public class TrafficLight extends Artifact {
         this.state = TrafficLightState.RED;
         getObsProperty("trState" + id).updateValue("red");
         log("Traffic Light is: " + this.state);
-        this.await_time(5_000);
+        this.waitAndSpawnVehicles(redWaitTime, 500, 50);
         this.sendRedIntersection();
 
         //TODO Send to intersection state
@@ -37,7 +52,7 @@ public class TrafficLight extends Artifact {
         this.state = TrafficLightState.YELLOW;
         getObsProperty("trState" + id).updateValue("yellow");
         log("Traffic Light is: " + this.state);
-        this.await_time(5_000);
+        this.await_time(yellowWaitTime);
 
         //TODO Send to intersection state
 //        try {
@@ -52,7 +67,7 @@ public class TrafficLight extends Artifact {
         this.state = TrafficLightState.GREEN;
         getObsProperty("trState" + id).updateValue("green");
         log("Traffic Light is: " + this.state);
-        this.await_time(5_000);
+        this.await_time(greenWaitTime);
 
         //TODO Send to intersection state
 //        try {
@@ -85,6 +100,22 @@ public class TrafficLight extends Artifact {
             execLinkedOp(intersection, "trIsDone", this.id);
         } catch (OperationException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void waitAndSpawnVehicles(final long timeToWaitMillis, final long checksPerMillis, int chanceToSpawnVehicles) {
+        int generatedNumber;
+        Random rnd = new Random();
+        for(long i = checksPerMillis; i < timeToWaitMillis; i += checksPerMillis) {
+            this.await_time(checksPerMillis);
+            generatedNumber = rnd.nextInt(1, 100);
+            if(generatedNumber < chanceToSpawnVehicles) {
+                this.lane.getVehiclesOnLane().add(new Vehicle(VehicleType.Normal));
+                ObsProperty normalCarsProp = getObsProperty("normalVehicles");
+                normalCarsProp.updateValue(normalCarsProp.intValue() + 1);
+
+                getObsProperty("totalVehicles").updateValue(getObsProperty("normalVehicles").intValue() + getObsProperty("emergencyVehicles").intValue());
+            }
         }
     }
 
