4241c033b3c98aaeff32313412c6a55e183df1fd
FPirazz <federicopirazzoli99@gmail.com>
2024-02-07 18:57:30 +0100

    feat: Update the User entity inside userContext to contain a password

--- src/main/userContext/src/main/java/com/userContext/business_logic_layer/UserRepository.java
+++ src/main/userContext/src/main/java/com/userContext/business_logic_layer/UserRepository.java
@@ -2,6 +2,10 @@ package com.userContext.business_logic_layer;
 
 import com.userContext.business_logic_layer.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+//    @Query("SELECT * FROM USER AS u WHERE u.name = ?1 AND u.surname = ?2 AND u.password = ?3")
+//    boolean findRegisteredUser(final String name, final String surname, final String password);
 }
