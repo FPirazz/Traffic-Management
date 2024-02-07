cc44793caeb67d545ac2ab408f80b1c9ab1777d8
FPirazz <federicopirazzoli99@gmail.com>
2024-02-07 17:42:58 +0100

    feat: Implemented Store module, that lets Vue store information in memory, kind of like cookies, for website use

--- tcm_frontend/src/main.js
+++ tcm_frontend/src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "@/router/router"
+import store from "@/store/store"
 
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap"
 
 const app = createApp(App)
 app.use(router)
+app.use(store)
 // app.use(BootstrapVue)
 
 
