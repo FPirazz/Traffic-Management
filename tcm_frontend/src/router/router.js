8a6ddfb14ad149cc22ac9e36a783f8c7c6507882
FPirazz <federicopirazzoli99@gmail.com>
2024-02-07 17:27:30 +0100

    feat: Added basic Login and Register pages, with their route, to further develop

--- tcm_frontend/src/router/router.js
+++ tcm_frontend/src/router/router.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
+import Register from "@/pages/creds/Register.vue";
+import Login from "@/pages/creds/Login.vue";
 
 const routes = [
     { path: "/", name: "Home", component: HomePage },
+    { path: "/user/register", name: "Register", component: Register},
+    { path: "/user/login", name: "Login", component: Login},
     { path: "/:catchAll(.*)", redirect: "/404" }
 ]
 
