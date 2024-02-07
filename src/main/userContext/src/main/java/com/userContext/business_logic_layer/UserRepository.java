4966564d3f2b212b806d5e399f2c4ed06a1d4240
FPirazz <federicopirazzoli99@gmail.com>
2024-02-07 20:05:42 +0100

    feat: Added findByCred function in UserController, in order to check wheter user exists or not

--- src/main/userContext/src/main/java/com/userContext/business_logic_layer/UserRepository.java
+++ src/main/userContext/src/main/java/com/userContext/business_logic_layer/UserRepository.java
@@ -4,8 +4,10 @@ import com.userContext.business_logic_layer.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    @Query("SELECT * FROM USER AS u WHERE u.name = ?1 AND u.surname = ?2 AND u.password = ?3")
-//    boolean findRegisteredUser(final String name, final String surname, final String password);
+    @Query(value = "SELECT u FROM User AS u WHERE u.name = ?1 AND u.surname = ?2 AND u.password = ?3")
+    Optional<User> findByCreds(final String name, final String surname, final String password);
 }
