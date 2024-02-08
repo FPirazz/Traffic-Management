d0a9537e4e0429a1719251b03b5e57d8900dd969
FPirazz <federicopirazzoli99@gmail.com>
2024-02-08 15:16:00 +0100

    feat: Added connection to JaCaMo-Rest system, to visualize status of traffic lights on frontend; Also implemented reverse-nginx proxy server for RPCs through Docker container

--- tcm_frontend/src/router/router.js
+++ tcm_frontend/src/router/router.js
@@ -3,12 +3,14 @@ import HomePage from "@/pages/HomePage.vue";
 import Register from "@/pages/creds/Register.vue";
 import Login from "@/pages/creds/Login.vue";
 import Intersections from "@/pages/intersection/Intersections.vue"
+import IntersectionPage from "@/pages/intersection/IntersectionPage.vue"
 
 const routes = [
     { path: "/", name: "Home", component: HomePage },
     { path: "/user/register", name: "Register", component: Register},
     { path: "/user/login", name: "Login", component: Login},
     { path: "/intersections", name: "Intersections", component: Intersections},
+    { path: "/intersections/1", name: "IntersectionPage", component: IntersectionPage},
 
     { path: "/logout", name: "Logout", component: HomePage },
     { path: "/:catchAll(.*)", redirect: "/404" }
