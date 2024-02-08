6466b249e7a849f158aacf9b014093600b9e20cf
FPirazz <federicopirazzoli99@gmail.com>
2024-02-08 12:42:34 +0100

    feat: Added another simple page to Navbar to simulate list of all available intersections to look at

--- tcm_frontend/src/router/router.js
+++ tcm_frontend/src/router/router.js
@@ -2,11 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/pages/HomePage.vue";
 import Register from "@/pages/creds/Register.vue";
 import Login from "@/pages/creds/Login.vue";
+import Intersections from "@/pages/intersection/Intersections.vue"
 
 const routes = [
     { path: "/", name: "Home", component: HomePage },
     { path: "/user/register", name: "Register", component: Register},
     { path: "/user/login", name: "Login", component: Login},
+    { path: "/intersections", name: "Intersections", component: Intersections},
+
     { path: "/logout", name: "Logout", component: HomePage },
     { path: "/:catchAll(.*)", redirect: "/404" }
 ]
