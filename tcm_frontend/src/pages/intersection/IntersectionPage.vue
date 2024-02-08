4802c1f09900550e5076a90def75b6f8b85393cf
FPirazz <federicopirazzoli99@gmail.com>
2024-02-08 18:16:12 +0100

    feat: Added pace visualization to the frontend page

--- tcm_frontend/src/pages/intersection/IntersectionPage.vue
+++ tcm_frontend/src/pages/intersection/IntersectionPage.vue
@@ -6,6 +6,7 @@
         <p v-if="tl1.state == 'red'" class="text-white bg-danger">State - {{ tl1.state }}</p>
         <p v-else-if="tl1.state == 'yellow'" class="bg-warning">State - {{ tl1.state }}</p>
         <p v-else class="text-white bg-success">State - {{ tl1.state }}</p>
+        <p>Pace - {{ tl1.pace }}</p>
         <p>Total Vehicles - {{ tl1.totalVehicles }}</p>
         <p>Normal Vehicles - {{ tl1.normalVehicles }}</p>
         <p>Emergency Vehicles - {{ tl1.emergencyVehicles }}</p>
@@ -17,6 +18,7 @@
         <p v-if="tl2.state == 'red'" class="text-white bg-danger">State - {{ tl2.state }}</p>
         <p v-else-if="tl2.state == 'yellow'" class="bg-warning">State - {{ tl2.state }}</p>
         <p v-else class="text-white bg-success">State - {{ tl2.state }}</p>
+        <p>Pace - {{ tl2.pace }}</p>
         <p>Total Vehicles - {{ tl2.totalVehicles }}</p>
         <p>Normal Vehicles - {{ tl2.normalVehicles }}</p>
         <p>Emergency Vehicles - {{ tl2.emergencyVehicles }}</p>
@@ -28,6 +30,7 @@
         <p v-if="tl3.state == 'red'" class="text-white bg-danger">State - {{ tl3.state }}</p>
         <p v-else-if="tl3.state == 'yellow'" class="bg-warning">State - {{ tl3.state }}</p>
         <p v-else class="text-white bg-success">State - {{ tl3.state }}</p>
+        <p>Pace - {{ tl3.pace }}</p>
         <p>Total Vehicles - {{ tl3.totalVehicles }}</p>
         <p>Normal Vehicles - {{ tl3.normalVehicles }}</p>
         <p>Emergency Vehicles - {{ tl3.emergencyVehicles }}</p>
@@ -39,6 +42,7 @@
         <p v-if="tl4.state == 'red'" class="text-white bg-danger">State - {{ tl4.state }}</p>
         <p v-else-if="tl4.state == 'yellow'" class="bg-warning">State - {{ tl4.state }}</p>
         <p v-else class="text-white bg-success">State - {{ tl4.state }}</p>
+        <p>Pace - {{ tl4.pace }}</p>
         <p>Total Vehicles - {{ tl4.totalVehicles }}</p>
         <p>Normal Vehicles - {{ tl4.normalVehicles }}</p>
         <p>Emergency Vehicles - {{ tl4.emergencyVehicles }}</p>
@@ -63,24 +67,28 @@ export default {
         normalVehicles: 0,
         emergencyVehicles: 0,
         state: "",
+        pace: "",
       },
       tl2: {
         totalVehicles: 0,
         normalVehicles: 0,
         emergencyVehicles: 0,
         state: "",
+        pace: "",
       },
       tl3: {
         totalVehicles: 0,
         normalVehicles: 0,
         emergencyVehicles: 0,
         state: "",
+        pace: "",
       },
       tl4: {
         totalVehicles: 0,
         normalVehicles: 0,
         emergencyVehicles: 0,
         state: "",
+        pace: "",
       },
     }
   },
@@ -94,36 +102,40 @@ export default {
         .then(res => {
           var content = res.data.properties;
           this.tl1.emergencyVehicles = content[1].emergencyVehicles[0];
-          this.tl1.normalVehicles = content[3].normalVehicles[0];
-          this.tl1.totalVehicles = content[2].totalVehicles[0];
-          this.tl1.state = content[4].trState1[0];
+          this.tl1.normalVehicles = content[4].normalVehicles[0];
+          this.tl1.totalVehicles = content[3].totalVehicles[0];
+          this.tl1.state = content[5].trState1[0];
+          this.tl1.pace = content[2].pace[0];
         });
 
         await axios.get("http://localhost:8085/tl2")
             .then(res => {
               var content = res.data.properties;
               this.tl2.emergencyVehicles = content[0].emergencyVehicles[0];
-              this.tl2.normalVehicles = content[3].normalVehicles[0];
-              this.tl2.totalVehicles = content[2].totalVehicles[0];
-              this.tl2.state = content[4].trState2[0];
+              this.tl2.normalVehicles = content[4].normalVehicles[0];
+              this.tl2.totalVehicles = content[3].totalVehicles[0];
+              this.tl2.state = content[5].trState2[0];
+              this.tl2.pace = content[2].pace[0];
             });
 
         await axios.get("http://localhost:8085/tl3")
             .then(res => {
               var content = res.data.properties;
               this.tl3.emergencyVehicles = content[1].emergencyVehicles[0];
-              this.tl3.normalVehicles = content[4].normalVehicles[0];
-              this.tl3.totalVehicles = content[3].totalVehicles[0];
+              this.tl3.normalVehicles = content[5].normalVehicles[0];
+              this.tl3.totalVehicles = content[4].totalVehicles[0];
               this.tl3.state = content[0].trState3[0];
+              this.tl3.pace = content[2].pace[0];
             });
 
         await axios.get("http://localhost:8085/tl4")
             .then(res => {
               var content = res.data.properties;
               this.tl4.emergencyVehicles = content[1].emergencyVehicles[0];
-              this.tl4.normalVehicles = content[4].normalVehicles[0];
-              this.tl4.totalVehicles = content[2].totalVehicles[0];
+              this.tl4.normalVehicles = content[5].normalVehicles[0];
+              this.tl4.totalVehicles = content[3].totalVehicles[0];
               this.tl4.state = content[0].trState4[0];
+              this.tl4.pace = content[2].pace[0];
             });
       } catch (err) {
         console.log(err);
