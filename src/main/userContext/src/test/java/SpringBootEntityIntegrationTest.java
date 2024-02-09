807ee20303a84aac713cb5b9dd29f6d8d23f2ad1
FPirazz <federicopirazzoli99@gmail.com>
2024-02-09 18:34:52 +0100

    test: Update user repo test class with more tests

--- src/main/userContext/src/test/java/SpringBootEntityIntegrationTest.java
+++ src/main/userContext/src/test/java/SpringBootEntityIntegrationTest.java
@@ -22,12 +22,33 @@ public class SpringBootEntityIntegrationTest {
     public void saveAndCheckRepoData() {
         User user = userRepository.save(new User("Test Name", "Test Surname", "Test Password", UserRole.Operator));
         Optional<User> searchedUser = userRepository.findById(user.getId());
-
         assertTrue(searchedUser.isPresent());
-        assertEquals(user.getId(), searchedUser.get().getId());
-        assertEquals(user.getName(), searchedUser.get().getName());
-        assertEquals(user.getSurname(), searchedUser.get().getSurname());
-        assertEquals(user.getPassword(), searchedUser.get().getPassword());
-        assertEquals(user.getRole(), searchedUser.get().getRole());
+        this.checkSavedData(user, searchedUser.get());
+        this.checkSetData(searchedUser.get());
+    }
+
+    private void checkSetData(User searchedUser) {
+        searchedUser.setId(5L);
+        assertEquals(searchedUser.getId(), 5L);
+
+        searchedUser.setName("New Name");
+        assertEquals(searchedUser.getName(), "New Name");
+
+        searchedUser.setSurname("New Surname");
+        assertEquals(searchedUser.getSurname(), "New Surname");
+
+        searchedUser.setPassword("New Password");
+        assertEquals(searchedUser.getPassword(), "New Password");
+
+        searchedUser.setRole(UserRole.Operator);
+        assertEquals(searchedUser.getRole(), UserRole.Operator);
+    }
+
+    private void checkSavedData(final User user, final User searchedUser) {
+        assertEquals(user.getId(), searchedUser.getId());
+        assertEquals(user.getName(), searchedUser.getName());
+        assertEquals(user.getSurname(), searchedUser.getSurname());
+        assertEquals(user.getPassword(), searchedUser.getPassword());
+        assertEquals(user.getRole(), searchedUser.getRole());
     }
 }
