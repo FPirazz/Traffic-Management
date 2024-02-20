1be56626f58c9f48f2a83d0ac7d370a390aad363
Lorenzo Massone <lorenzomassone@gmail.com>
2024-02-20 18:17:24 +0100

    fix: Fix toolchain JDK in build.gradle.kts for userContext

--- src/main/userContext/build.gradle.kts
+++ src/main/userContext/build.gradle.kts
@@ -28,9 +28,6 @@ plugins {
 }
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(19)
-    }
     sourceCompatibility = JavaVersion.VERSION_19
     targetCompatibility = JavaVersion.VERSION_19
 }
